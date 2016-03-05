96f79113c9940d2cf6551a18af8b4df5b871ce44
alochym01 <donguyenha@gmail.com>
2016-03-05 22:54:53 +0700

    save info net data into json file

--- info_net.py
+++ info_net.py
@@ -4,6 +4,7 @@ import shlex
 import os
 import re
 import wget
+import json
 from subprocess import Popen, PIPE
 
 def mysoup(link):
@@ -11,6 +12,15 @@ def mysoup(link):
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page.read(), "html5lib")
     return soup
+
+def save(filename, data):
+    with open(filename, 'w') as f:
+        f.write(','.join(set(data)))
+
+def read(filename):
+    with open(filename) as f:
+        data = f.read().split(',')
+    return data
 
 url = 'http://infonet.vn/kinh-doanh/4.info'
 
@@ -27,46 +37,39 @@ for i in wrap.find_all('a'):
             links.append(i.get('href'))
     except:
         pass
-#remove duplicate items
-#print list(set(links))
-
 
 #save temp value into tem file
-with open('info_net_temp.txt', 'w') as f:
-    f.write(','.join(set(links)))
-
-
+save('info_net_temp.txt', links)
 
 #begin to compare a links value(info_net.txt) with links temp value(info_net_temp)
-with open('info_net.txt') as f:
-    try:
-        links_from_file = f.read().split(',')
-    except:
-        links_from_file = []
-        pass
+links_from_file = read('info_net.txt')
 
-with open('info_net_temp.txt') as f:
-    try:
-        links_temp = f.read().split(',')
-    except:
-        links_temp = []
-        pass
+links_temp = read('info_net_temp.txt')
 
+#remove duplicate items
 if len(list(set(links_temp) - set(links_from_file))):
     print list(set(links_temp) - set(links_from_file))
-    with open('info_net.txt', 'w') as f:
-        f.write(','.join(set(links)))
+    save('info_net.txt', links)
 
 #begin crawler content of link
-soup = mysoup(url)
+for url in list(set(links_temp) - set(links_from_file)):
+    soup = mysoup('http://infonet.vn/' + url)
 
-article = soup.find("article")
-tags_p =[]
-img = article.find('img').get('src')
+    article = soup.find("article")
+    tags_p =[]
+    img = article.find('img').get('src')
 
-for p in article.find_all('p'):
-    tags_p.append(p)
+    for p in article.find_all('p'):
+        print type(p)
+        tags_p.append(p)
 
-#update video json file with img + data
-video['items'][0]['snippet']['content'].update({'img': img})
-video['items'][0]['snippet']['content'].update({'data': tags_p})
+    with open('videos.json') as f:
+        video = json.loads(f.read())
+    #update video json file with img + data
+    video['items'][0]['snippet']['img'] = img
+    video['items'][0]['snippet']['data'] = []
+    for i in tags_p:
+        video['items'][0]['snippet']['data'].append(i.string)
+    with open('/home/hadn' + url, 'w') as f:
+        f.write(json.dumps(video,indent=1))
+    #print tags_p
